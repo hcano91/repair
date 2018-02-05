a00d3c24b8543d88c655e6193018b35286d77cc3
Hector Cano <hector.cano@base22.com>
2018-02-05 09:05:22 -0500

    commit left out

--- hospital-project/src/app/got-user-info/got-user-info.guard.ts
+++ hospital-project/src/app/got-user-info/got-user-info.guard.ts
@@ -3,18 +3,23 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Resolve, Router } from '@angular/router';
 import { AngularFireAuth } from "angularfire2/auth";
 import { Subscription } from 'rxjs/Subscription';
+import { DataService } from '../services/data.service';
+import { SystemState } from '../services/systemState.service';
 
 @Injectable()
 export class GotUserInfoGuard implements Resolve<any> {
   constructor(
     private readonly afAuth: AngularFireAuth, 
-    private readonly router: Router,) {
+    private readonly router: Router,
+    private dataService: DataService,
+    private systemState: SystemState) {
   }
-  resolve(): Subscription {
-    return this.afAuth.authState.subscribe(data => {
-      if(data === null){
-        this.router.navigate(['login']);
-      }
-    });
+  resolve(): Promise<any> {
+
+    return this.dataService.getUserDataQuery(this.systemState.uid)
+          .then((data)=>{console.log(data);this.systemState.setUserData(data);})
+          .catch((error)=>{this.router.navigate(['login']);})
+      
+  
   }
 }
