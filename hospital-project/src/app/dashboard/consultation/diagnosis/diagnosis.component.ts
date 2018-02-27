e39f2e2d83d51be468f0d9779442b09b76291e2f
Hector Cano <hector.cano@base22.com>
2018-02-27 11:58:14 -0500

    finished diagnosis section

--- hospital-project/src/app/dashboard/consultation/diagnosis/diagnosis.component.ts
+++ hospital-project/src/app/dashboard/consultation/diagnosis/diagnosis.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ConsultationService } from  '../../../services/consultation.service';
+import { NgForm } from '@angular/forms';
+import { ProvideConsultationFormDirective } from '../../../directives/provide-consultation-form.directive';
 
 @Component({
   selector: 'app-diagnosis',
@@ -7,7 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiagnosisComponent implements OnInit {
 
-  constructor() { }
+  constructor(public consultationService: ConsultationService) { }
+
+  canShow(previousInput) {
+    if(typeof previousInput === 'undefined') {
+      return true;
+    }
+
+    if(typeof previousInput.value === 'undefined') {
+      return true;
+    }
+
+    if(previousInput === null) {
+      return true;
+    }
+
+    if(previousInput.value === null) {
+      return true;
+    }
+
+    if(previousInput.value.length === 0) {
+      return true;
+    }
+
+    return false;
+  }
 
   ngOnInit() {
   }
